c90c49e4ade3200bbdd328f0aa557147052c0998
BTKgamer <71685920+BTKcreations@users.noreply.github.com>
2024-01-24 17:17:18 +0530

    todolist app react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import './App.css'
+import './index.css'
+import React, { useState } from 'react';
+import TodoList from './components/TodoList';
+import AddTodoForm from './components/AddTodoForm';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text }]);
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO App</h1>
+      <AddTodoForm onAdd={addTodo} />
+      <TodoList todos={todos} onDelete={deleteTodo} />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+// src/components/TodoList.js
+import React from 'react';
+
+const TodoList = ({ todos, onDelete }) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => onDelete(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
